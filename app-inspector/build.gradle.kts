f3e0743f89c976675dbae5960fbe10a037d85e77
totalnybet-create <totalnybet@gmail.com>
2026-08-20 11:01:27 +0200

    p0: add Android app inspector module

---
+++ app-inspector/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "pl.siedlar.securityguardian.inspector"
+    compileSdk = 36
+
+    defaultConfig {
+        minSdk = 23
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation(project(":core-security"))
+}
